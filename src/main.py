a4ad3540b4aa175f87b629e025e1cc07712c55d0
unknown <nattapatnillapun@gmail.com>
2026-01-08 19:14:21 +0700

    Add initial Pygame main loop

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
-#blank for main
+import pygame
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Jaimer Game Project")
+
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((30,30,30))
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
