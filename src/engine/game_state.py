6bf19881c3eb5c873f5c6a4b35bdfad217a42d07
unknown <nattapatnillapun@gmail.com>
2026-01-09 17:41:04 +0700

    reconstruct main.py and game_state.py

---
+++ src/engine/game_state.py
@@ -0,0 +1,13 @@
+class gameState:
+
+    def __init__(self, game):
+        self.game = game
+        self.prev_state = None
+
+    def render(self, delta_time, action):
+        pass
+
+    def update(self, surface):
+        pass
+
+    
--- src/main.py
+++ src/main.py
@@ -1,24 +1,85 @@
-import pygame
+import pygame, time, os
 
-def main():
-    pygame.init()
+class Game:
 
-    screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption("Jaimer Game Project")
+    def __init__(self):
+        pygame.init()
+        self.Game_W, self.Game_H = 480, 270
+        self.SCREEN_WIDTH, self.SCREEN_HEIGHT = 960, 540
+        self.gameCanvas = pygame.Surface((self.Game_W, self.Game_H))
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.running, self.playing = True, True
+        self.action = {"up": False, "down": False, "left": False, "right": False, "escape": False, "return": False}
+        self.dt, self.prev_time = 0, 0
+        self.state_stack = []
+        self.load_assets()
 
-    clock = pygame.time.Clock()
-    running = True
 
-    while running:
+    def get_event(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                self.running, self.playing = False, False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    self.action["up"] = True
+                if event.key == pygame.K_s:
+                    self.action["down"] = True
+                if event.key == pygame.K_a:
+                    self.action["left"] = True
+                if event.key == pygame.K_d:
+                    self.action["right"] = True
+                if event.key == pygame.K_ESCAPE:
+                    self.action["escape"] = True
+                if event.key == pygame.K_RETURN:
+                    self.action["return"] = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    self.action["up"] = False
+                if event.key == pygame.K_s:
+                    self.action["down"] = False
+                if event.key == pygame.K_a:
+                    self.action["left"] = False
+                if event.key == pygame.K_d:
+                    self.action["right"] = False
+                if event.key == pygame.K_ESCAPE:
+                    self.action["escape"] = False
+                if event.key == pygame.K_RETURN:
+                    self.action["return"] = False
+    def update(self):
+        pass
 
-        screen.fill((30,30,30))
+    def render(self):
+        self.screen.blit(pygame.transform.scale(self.gameCanvas, (self.Game_W, self.Game_H)), (0, 0))
         pygame.display.flip()
-        clock.tick(60)
 
-    pygame.quit()
+    def get_dt(self):
+        now = time.time()
+        self.dt = now - self.prev_time
+        self.prev_time = now
+
+    def draw_text(self, surface, text, color, x_pos, y_pos):
+        text_surface = self.font.render(text, True, color)
+        #text_surface.set_colorkey((0,0,0))
+        text_rect = text_surface.get_rect()
+        text_rect.center = (x_pos, y_pos)
+        surface.blit(text_surface, text_rect)
+
+    def load_assets(self):
+        # Create pointers to directories
+        self.assets_dir = os.path.join("assets")
+        self.sprites_dir = os.path.join(self.assets_dir, "sprites")
+        self.font_dir = os.path.join(self.assets_dir, "fonts")
+        #self.font = pygame.font.Font(os.path.join(self.font_dir, ""))
+        self.font = pygame.font.SysFont("Consolas", 24)
+
+    def mainloop(self):
+        while self.playing:
+            self.get_dt()
+            self.get_event()
+            self.update()
+            self.render()
 
 if __name__ == "__main__":
-    main()
+    game = Game()
+    if game.running:
+        game.mainloop()
